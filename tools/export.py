9e6b23c42c4b8c659e985fcd8f335075e950833e
Pixelpunker <ams_reg+gh2@mailbox.org>
2022-02-09 20:22:09 +0100

    Export whole map

--- tools/export.py
+++ tools/export.py
@@ -3,7 +3,9 @@ filename = "assets.hpp"
 with open("assets/celeste.p8", 'rb') as fh:
   data = get_raw_data_from_p8_file(fh, filename="celeste.p8")
 # print(data.section_lines['gfx'][1])
-converted = from_lines(data.section_lines['map'], version=data.version)
+converted = from_lines(data.section_lines['map'], 
+	data.section_lines['gfx'], 
+	version=data.version)
 file1 = open("assets/"+filename, "w")
 file1.write(converted)
 file1.close
